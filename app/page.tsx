e8761ece9c33bd3bb646385bac6d311610ba4d15
Erik Young <erik_young@mac.com>
2024-02-15 17:43:22 -0500

    Changed image aspect ratio.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const frameMetadata = getFrameMetadata({
   ],
   image: {
     src: `${NEXT_PUBLIC_URL}/jumper-image-1.png`,
-    aspectRatio: '1:1',
+    aspectRatio: '1.91:1',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
